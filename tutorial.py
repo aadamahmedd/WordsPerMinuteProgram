6d722b5ab475eeefe6888ed651f958ca162bc7a2
aadamahmedd <90535188+aadamahmedd@users.noreply.github.com>
2023-04-17 02:00:49 -0400

    Add files via upload

--- tutorial.py
+++ tutorial.py
@@ -1,5 +1,66 @@
 import curses
 from curses import wrapper
+import time
 
-def main(stdscr):
+def startScreen(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Welcome to the Speed Typing Test")
+    stdscr.addstr("\nPress any key to begin!")
+    stdscr.refresh()
+    stdscr.getkey()
     
+def display_text(stdscr, target, current, wpm=0):
+    stdscr.addstr(target)
+    stdscr.addstr(1, 0, f"WPM: {wpm}")
+    
+    for i, char in enumerate(current):
+        correct_char = target[i]
+        color = curses.color_pair(1)
+        if char != correct_char:
+            color = curses.color_pair(2)
+            
+        stdscr.addstr(0, i, char, color)  
+    
+    
+def wpmTest(stdscr):
+    target_text = "Hello world this is some test text for this app!"
+    current_text = []
+    wpm = 0
+    start_time = time.time()
+    stdscr.nodelay(True)
+    
+    while True:
+        time_elapsed = max(time.time() - start_time, 1)
+        wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
+        
+        stdscr.clear()
+        display_text(stdscr, target_text, current_text, wpm)
+        stdscr.refresh()
+        
+        try:
+            key = stdscr.getkey()
+        except:
+            continue
+        
+        if ord(key) == 27:
+            break
+        
+        if key in ("KEY_BACKSPACE", '\b', '\x7f'):
+            if len(current_text) > 0:
+                current_text.pop()
+        
+        elif len(current_text) < len(target_text):    
+            current_text.append(key)
+        
+        
+    
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+    
+    startScreen(stdscr)
+    wpmTest(stdscr)
+    
+wrapper(main)
